518bb6e03b776fa7341e256de15534ffb810a939
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-28 13:51:37 -0700

    created multiple basic routes

--- flaskblog.py
+++ flaskblog.py
@@ -4,4 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Home Page</h1>'
+
+
+@app.route('/home')
+def home():
+    return '<h1>Home Page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
