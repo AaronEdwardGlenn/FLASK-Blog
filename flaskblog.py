12f1cac4d4408eedb6c11ebc60aa32e54c097563
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-30 19:05:31 -0700

    importing SQLAlchemy for db creation locally

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from flask import Flask, render_template, url_for, flash, redirect
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4168461357cf940173596c551cc9ea6d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 posts = [
     {
