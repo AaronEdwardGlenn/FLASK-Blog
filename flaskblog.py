54ca22a0aede02afa5a88ddc113abca5dfe6c7a7
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-30 18:17:40 -0700

    alert message on user registration and redirect working

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from forms import RegistrationForm, LoginForm
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4168461357cf940173596c551cc9ea6d'
@@ -31,9 +31,12 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
