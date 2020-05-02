fc8cf4b48f6618b61049fd71a6f3441f66422d37
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-05-01 17:06:42 -0700

    had to fix autoindent on routes.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,10 @@
-from flaskblog import routes
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4168461357cf940173596c551cc9ea6d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,49 +1,52 @@
-from flaskblog.models import User, Post
-from flaskblog import render_template, url_for, flash, redirect
-from formsblog.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect
 from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 
 posts = [
     {
-        'author': 'Aaron Glenn',
-        'title': 'Blog 2',
-        'content': 'First Content',
-        'date_posted': 'April 28, 2020'
-        },
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
     {
-        'author': 'Aaron Edward',
-        'title': 'Blog 2',
-        'content': 'Second Content',
-        'date_posted': 'April 28, 2020'
-        }
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html', title='About')
 
-        @app.route('/register', methods=['POST', 'GET'])
-        def register():
-            form = RegistrationForm()
-            if form.validate_on_submit():
-                flash(f'Account created for {form.username.data}!', 'success')
-                return redirect(url_for('home'))
-                return render_template('register.html', title='Register', form=form)
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
-                @app.route('/login', methods=['POST', 'GET'])
-                def login():
-                    form = LoginForm()
-                    if form.validate_on_submit():
-                        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-                            flash('You have been logged in', 'success')
-                            return redirect(url_for('home'))
-                        else:
-                            flash('Login Unsucessful. Please check username and password', 'danger')
-                            return render_template('login.html', title='Login', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
